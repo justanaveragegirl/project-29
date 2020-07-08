853065c7892556f8c65365a12853c72f5c304886
justanaveragegirl <64732700+justanaveragegirl@users.noreply.github.com>
2020-07-08 19:41:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var bin1,bin2,bin3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+    bin1=new Bin(100,10,20,30);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	//World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  bin1.display();
+  drawSprites();
+ 
+}
+
+
+
